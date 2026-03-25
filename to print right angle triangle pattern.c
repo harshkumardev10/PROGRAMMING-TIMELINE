33b5da8fe34c8bd9655f4ba5269e20efc23a330e
HARSH KUMAR <harshkumar.dev10@gmail.com>
2026-03-25 21:38:20 +0530

    Add files via upload

---
+++ to print right angle triangle pattern.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int N;
+    printf("Enter value of row :");
+    scanf("%d", &N);
+    for(int i=1; i<N; i++){
+        for(int j=0; j<i; j++ ){
+        printf("* ");
+    }
+    printf("\n");
+}
+return 0;
+}
